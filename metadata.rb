5dca2e1b8b9c7e27310d374ea6e2b5b0af916305
Jean Jacques Warmerdam <sjaq@koelka.st>
2013-11-12 14:55:39 +0100

    Update version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ license          "Apache 2.0"
 description      "Installs/Configures gitlab"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 name             "gitlab"
-version          "6.1.0"
+version          "6.2.0"
 %w[ build-essential zlib readline ncurses git openssh redisio xml
     python ruby_build sudo certificate nginx database mysql
     postgresql yum apt ].each do |cb_depend|
